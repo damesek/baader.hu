c77b171b3a66fb8e8b4dd135804215fcff37c241
Bader Szabolcs <sz@baader.hu>
2023-11-03 20:28:44 +0100

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+# Baader.hu personal public site
+
+My personal website uses a Tailwind UI template, which was built using Tailwind CSS and Next.js.
+
+WIP:
+- missing mainly all blogpost etc
+
+
+## Getting started
+
+To get started with this template, first install the npm dependencies:
+
+```bash
+yarn
+```
+
+Next, create a `.env.local` file in the root of your project and set the `NEXT_PUBLIC_SITE_URL` variable to your site's public URL:
+
+```
+NEXT_PUBLIC_SITE_URL=https://example.com
+```
+
+Next, run the development server:
+
+```bash
+npm run dev
+```
+
+Finally, open [http://localhost:3000](http://localhost:3000) in your browser to view the website.
+
+
+## License
+
+You can't use any part of this commercially, based on Tailwind UI license.
+
---
+++ src/app/about/page.tsx
@@ -0,0 +1,106 @@
+import { type Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+import clsx from 'clsx'
+
+import { Container } from '@/components/Container'
+import {
+  GitHubIcon,
+  InstagramIcon,
+  LinkedInIcon,
+  TwitterIcon,
+} from '@/components/SocialIcons'
+import portraitImage from '@/images/budapest.png'
+
+function SocialLink({
+  className,
+  href,
+  children,
+  icon: Icon,
+}: {
+  className?: string
+  href: string
+  icon: React.ComponentType<{ className?: string }>
+  children: React.ReactNode
+}) {
+  return (
+    <li className={clsx(className, 'flex')}>
+      <Link
+        href={href}
+        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+      >
+        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+        <span className="ml-4">{children}</span>
+      </Link>
+    </li>
+  )
+}
+
+function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
+      />
+    </svg>
+  )
+}
+
+export const metadata: Metadata = {
+  title: 'About',
+  description:
+    'I’m Baader Szabolcs. I live in Budapest, where I design the digital future.',
+}
+
+export default function About() {
+  return (
+    <Container className="mt-16 sm:mt-32">
+      <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+        <div className="lg:pl-20">
+          <div className="max-w-xs px-2.5 lg:max-w-none">
+            <Image
+              src={portraitImage}
+              alt=""
+              sizes="(min-width: 1024px) 32rem, 20rem"
+              className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+            />
+          </div>
+        </div>
+        <div className="lg:order-first lg:row-span-2">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+            I’m Baader Szabolcs. I'm live in Budapest, where I design the
+            new future's first seeds.
+          </h1>
+          <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+            <p>
+            I've been working in the world of computers, websites, and helping businesses grow for nearly twenty years. It's been a long and exciting journey, starting from a beginner to becoming a leader in my field. 
+            </p>
+            <p>
+            My expertise spans across data analytics and Conversion Rate Optimization (CRO), with a distinct ability to bridge communication between developers and the business realm. My development technology stack is rooted in Clojure and Python, showcasing my technical acumen in the field.
+            </p>
+            <p>
+            I am also very interested in something called Artificial Intelligence (AI), which is like teaching computers to think and learn like humans. It's amazing to see how AI can help online shops sell more products and understand what customers like and other parts of this field.
+            </p>
+            <p>
+            I love thinking of new ways to solve problems, making rough sketches of ideas (that's called prototyping), and seeing those ideas come to life. It's all about helping these small and big companies find their way to success, and it feels great to see them thrive!
+            </p>
+          </div>
+        </div>
+        <div className="lg:pl-20">
+          <ul role="list">
+            <SocialLink href="https://twitter.com/baaders" icon={TwitterIcon}>
+              Follow on Twitter
+            </SocialLink>
+            <SocialLink href="https://github.com/damesek" icon={GitHubIcon} className="mt-4">
+              Follow on GitHub
+            </SocialLink>
+            <SocialLink href="https://www.linkedin.com/in/baaderszabolcs/" icon={LinkedInIcon} className="mt-4">
+              Follow on LinkedIn
+            </SocialLink>
+          </ul>
+        </div>
+      </div>
+    </Container>
+  )
+}
